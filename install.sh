4ef958ba8a0a5f6e7bfef89e1a4ec052e462b615
Mario Semeš <mario.semes@gmail.com>
2019-09-28 20:10:12 +0200

    Features
    Added new features
    Added install.sh for easy installation

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+### Checking if tools are installed
+if ! [ -x "$(command -v youtube-dl)" ]; then
+  echo $"Error: youtube-dl is not installed."
+  echo $"Please, install the tool as mentioned in the installation guide."
+  ecto $"or try to install it: sudo apt-get install youtube-dl -y"
+fi
+
+if ! [ -x "$(command -v aria2c)" ]; then
+  echo $"Error: aria2c is not installed."
+  echo $"Please, install the tool as mentioned in the installation guide."
+  ecto $"or try to install it: sudo apt-get install aria2 -y"
+fi
+
+### Moving files
+mkdir $HOME/phdler
+echo $"phdler folder created"
+cp phdler.sh $HOME/phdler/phdler.sh
+cp phdler.config $HOME/phdler/phdler.config
+echo $"script & config moved"
+touch $HOME/phdler/stars.txt
+touch $HOME/phdler/stars-new.txt
+touch $HOME/phdler/models.txt
+touch $HOME/phdler/models-new.txt
+echo $"files created"
+echo $"-----------------"
+echo $"Script installed."
+echo $"-----------------"
+echo $"Please run this command as admin and dont forget to rename .your-user-name.:"
+echo $"sudo ln -s /home/your-user-name/phdler/phdler.sh /usr/local/bin/phdler"
+echo $"-----------------"
--- phdler.sh
+++ phdler.sh
@@ -9,14 +9,34 @@ tag=$3
 source $HOME/phdler/phdler.config
 
 ### Start script
-if [ "$action" == 'refresh' ]
+
+if [ "$action" == 'start' ]
+	then	
+		filename=$HOME/phdler/models.txt
+		filelines=`cat $filename`
+		echo Starting models
+		for line in $filelines ; do
+		       youtube-dl -w -v -i --playlist-start 1 --playlist-end $lastvideos --external-downloader aria2c --external-downloader-args '--file-allocation=none -c -j 10 -x 16 --summary-interval=0' -o $dllocation'/'$line'/%(title)s.%(ext)s' 'https://www.pornhub.com/model/'$line'/videos/upload'
+		done
+
+		filenameps=$HOME/phdler/stars.txt
+		filelinesps=`cat $filenameps`
+		echo Starting stars
+		for lineps in $filelinesps ; do
+		        youtube-dl -w -v -i --playlist-start 1 --playlist-end $lastvideos --external-downloader aria2c --external-downloader-args '--file-allocation=none -c -j 10 -x 16 --summary-interval=0' -o $dllocation'/'$lineps'/%(title)s.%(ext)s' 'https://www.pornhub.com/pornstar/'$lineps'/videos/upload'
+		done
+
+		echo Script run `date "+%H:%M:%S   %d/%m/%y"` >> $HOME/phdler.log
+
+
+elif [ "$action" == 'refresh' ]
 	then
-		filename="$filelocation"models-new.txt
+		filename=$HOME/phdler/models-new.txt
 		filelines=`cat $filename`
 
 		for line in $filelines ; do
 		       echo Searching for $line
-		       if grep -Fxq "$line" "$filelocation"models.txt
+		       if grep -Fxq "$line" $HOME/phdler/models.txt
 			       then
 			       	echo "Model exists. Do you wish to download it anyways?"
 					select yn in "yes" "no"; do
@@ -27,17 +47,17 @@ if [ "$action" == 'refresh' ]
 					done
 			       else
 			       	youtube-dl -w -v -i --external-downloader aria2c --external-downloader-args '--file-allocation=none -c -j 10 -x 16 --summary-interval=0' -o $dllocation'/'$line'/%(title)s.%(ext)s' 'https://www.pornhub.com/model/'$line'/videos/upload'
-			       	echo $line >> "$filelocation"models.txt
+			       	echo $line >> $HOME/phdler/models.txt
 			       	echo $line added to models.txt
 			   fi
 		done
 
-		filenameps="$filelocation"stars-new.txt
+		filenameps=$HOME/phdler/stars-new.txt
 		filelinesps=`cat $filenameps`
 
 		for lineps in $filelinesps ; do
 		        echo Searching for $lineps
-		        if grep -Fxq "$lineps" "$filelocation"stars.txt
+		        if grep -Fxq "$lineps" $HOME/phdler/stars.txt
 			       then
 			       	echo "Pornstar exists. Do you wish to download it anyways?"
 					select yn in "yes" "no"; do
@@ -48,72 +68,124 @@ if [ "$action" == 'refresh' ]
 					done
 			       else
 			       	youtube-dl -w -v -i --external-downloader aria2c --external-downloader-args '--file-allocation=none -c -j 10 -x 16 --summary-interval=0' -o $dllocation'/'$lineps'/%(title)s.%(ext)s' 'https://www.pornhub.com/pornstar/'$lineps'/videos/upload'
-			       	echo $lineps >> "$filelocation"stars.txt
+			       	echo $lineps >> $HOME/phdler/stars.txt
 			       	echo $lineps added to stars.txt
 			   fi
 		done
-
-		echo updated >> $updatefile
 
 elif [ "$action" == 'custom' ]
 	then
 		if [ "$command" == '' ]
 		then
-			echo $"Please run phdler custom <ph url>"
+			clear
+			echo $"-----------------"
+			echo $"Please run [phdler custom <ph url>]"
 			exit 1;
 		else
 		youtube-dl -w -v -i --external-downloader aria2c --external-downloader-args '--file-allocation=none -c -j 10 -x 16 --summary-interval=0' -o $dllocation'/handpicked/%(title)s.%(ext)s' $command
-		echo `date` >> $updatefile
 		fi
-
-elif [ "$action" == 'start' ]
-	then 
-		rm $updatefile		
-		filename="$filelocation"models.txt
-		filelines=`cat $filename`
-		
-		echo Starting models
-		for line in $filelines ; do
-		       echo Searching for $line - ${currentdate} >> $updatefile
-		       youtube-dl -w -v -i --playlist-start 1 --playlist-end 4 --external-downloader aria2c --external-downloader-args '--file-allocation=none -c -j 10 -x 16 --summary-interval=0' -o $dllocation'/'$line'/%(title)s.%(ext)s' 'https://www.pornhub.com/model/'$line'/videos/upload'
-		done
-
-		filenameps="$filelocation"stars.txt
-		filelinesps=`cat $filenameps`
-		echo Starting stars
-		for lineps in $filelinesps ; do
-		        echo Searching for $lineps - ${currentdate} >> $updatefile
-		        youtube-dl -w -v -i --playlist-start 1 --playlist-end 4 --external-downloader aria2c --external-downloader-args '--file-allocation=none -c -j 10 -x 16 --summary-interval=0' -o $dllocation'/'$lineps'/%(title)s.%(ext)s' 'https://www.pornhub.com/pornstar/'$lineps'/videos/upload'
-		done
 
 elif [ "$action" == 'add' ]
 	then
 		if [ "$command" == 'model' ]
 			then
-				if grep -Fxq "$tag" "$filelocation"models.txt
+				if grep -Fxq "$tag" $HOME/phdler/models.txt
 				then
 					echo Model exists.
 					exit 1;
 				else
-					echo $tag >> "$filelocation"models-new.txt
+					echo $tag >> $HOME/phdler/models-new.txt
 					echo $"Model added."
 					exit 1;
 				fi
 		elif [ "$command" == 'pornstar' ]
 			then
-				if grep -Fxq "$tag" "$filelocation"stars.txt
+				if grep -Fxq "$tag" $HOME/phdler/stars.txt
 				then
 					echo Pornstar exists.
 					exit 1;
 				else
-					echo $tag >> "$filelocation"stars-new.txt
+					echo $tag >> $HOME/phdler/stars-new.txt
 					echo $"Pornstar added."
 					exit 1;
 				fi
 		else
-				echo $"Please run phdler add model/pornstar"
+				clear
+				echo $"-----------------"
+				echo $"Please run [phdler add model/pornstar]"
 				exit 1;
 		fi
+
+elif [ "$action" == 'clean' ]
+	then
+		if [ "$command" == 'models' ]
+			then
+				> $HOME/phdler/models-new.txt
+				echo $"models-new.txt cleaned."
+				exit 1;
+				
+		elif [ "$command" == 'pornstars' ]
+			then
+				> $HOME/phdler/stars-new.txt
+				echo $"stars-new.txt cleaned."
+				exit 1;
+				
+		else
+				clear
+				echo $"-----------------"
+				echo $"Please run [phdler clean models/pornstars]"
+				exit 1;
+		fi
+
+elif [ "$action" == 'remove' ]
+	then
+		if [ "$command" == 'model' ]
+			then
+				clear
+				echo $"Listing of models:"
+				echo $"-----------------"
+				cat $HOME/phdler/models.txt
+				echo $"-----------------"
+				read -p 'Write the model name to remove (or type c to cancel): ' modelname
+
+				if [ "$modelname" != 'c' ]
+					then
+						grep -v "$modelname" $HOME/phdler/models.txt > $HOME/phdler/models2.txt; mv $HOME/phdler/models2.txt $HOME/phdler/models.txt
+						echo Deleted $modelname
+						exit 1;
+					else
+						echo Canceling operation.
+						exit 1;
+				fi
+				
+		elif [ "$command" == 'pornstar' ]
+			then
+				clear
+				echo $"Listing of pornstars:"
+				echo $"-----------------"
+				cat $HOME/phdler/stars.txt
+				echo $"-----------------"
+				read -p 'Write the pornstar name to remove (or type c to cancel): ' starname
+
+				if [ "$starname" != 'c' ]
+					then
+						grep -v "$modelname" $HOME/phdler/stars.txt > $HOME/phdler/stars2.txt; mv $HOME/phdler/stars2.txt $HOME/phdler/stars.txt
+						echo Deleted $starname
+						exit 1;
+					else
+						echo Canceling operation.
+						exit 1;
+				fi
+		else
+			echo $"-----------------"
+			echo $"Please run [phdler remove model/pornstar]"
+			exit 1;
+		fi
+
+elif [ "$action" == 'config' ]
+	then
+		nano $HOME/phdler/phdler.config
+		exit 1;
 
 elif [ "$action" == 'update' ]
 	then
@@ -122,15 +194,28 @@ elif [ "$action" == 'update' ]
 
 elif [ "$action" == '-h' ]
 	then
-		echo $"You asked for help, here it comes!"
-		echo $"$ phdler start (to start the script with already added models/pornstars)"
-		echo $"$ phdler refresh (to run the script and add/download the fresh database)"
-		echo $"$ phdler custom *url* (to download a custom URL from PornHub)"
-		echo $"$ phdler add *model or pornstar* (to add a new model or pornstar to the database)"
-		echo $"$ phdler update (to update youtube-dl to the latest version. Root permissions maybe needed)"
+		clear
+		echo $"-----------------"
+		echo $"You asked for help, here it comes! Run phdler with these commands:"
+		echo $"-----------------"
+		echo $"start (to start the script with already added models/pornstars)"
+		echo $"refresh (to run the script and download from the fresh database)"
+		echo $"custom *url* (to download a custom URL from PornHub)"
+		echo $"add *model or pornstar* (to add a new model or pornstar to the database)"
+		echo $"remove *model or pornstar* (to remove a model or pornstar from the database)"
+		echo $"clean *models or pornstars* (to clean the -new.txt database)"
+		echo $"config (to edit the config file)"
+		echo $"update (to update youtube-dl to the latest version. Root permissions maybe needed)"
+		echo $"-----------------"
+		echo $"Example: phdler add model here-goes-the-model-name"
+		echo $"-----------------"
 		exit 1;
 
 else
-		echo $"Please run phdler with these commands: start, refresh, custom, add, update or -h for help!"
+		clear
+		echo $"-----------------"
+		echo $"Please run phdler with these commands:"
+		echo $"[start | refresh | custom | add | remove | clean | update | config | -h for help]"
+		echo $"-----------------"
 		exit 1;
 fi
