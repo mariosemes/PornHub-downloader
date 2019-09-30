81f371715de29bfbb18094ae6067de65d80e8947
Mario Semeš <mario.semes@gmail.com>
2019-09-30 08:27:53 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,13 +4,11 @@
 if ! [ -x "$(command -v youtube-dl)" ]; then
   echo $"Error: youtube-dl is not installed."
   echo $"Please, install the tool as mentioned in the installation guide."
-  ecto $"or try to install it: sudo apt-get install youtube-dl -y"
 fi
 
 if ! [ -x "$(command -v aria2c)" ]; then
   echo $"Error: aria2c is not installed."
   echo $"Please, install the tool as mentioned in the installation guide."
-  ecto $"or try to install it: sudo apt-get install aria2 -y"
   exit 1;
 fi
 
